db3c7acf25eec15cd7c94ecc5a6ef9ca63aa19b9
Damian Grembocki <damian.grembocki@o2.pl>
2023-02-20 20:37:18 +0100

    commit

--- AdjustableSize_ArrayRandomFiller/Program.cs
+++ AdjustableSize_ArrayRandomFiller/Program.cs
@@ -1,9 +1,18 @@
 ﻿int arraySize;
+int foundNameCounter = 0;
+String nameToFound;
+String againFounder;
+
+Boolean foundNameAgain = true;
+
+String[] namesArray = new String[] { "damian", "stanisław", "karol", "bartosz", "dariusz", "tomasz",
+                "daniel", "jakub", "wojciech", "piotr"};
+
 try
 {
     do
     {
-        Console.WriteLine("Enter array size: ");
+        Console.Write("Enter array size: ");
         arraySize = Convert.ToInt32(Console.ReadLine());
         if (arraySize <= 0)
         {
@@ -11,26 +20,52 @@ try
         }
     } while (arraySize <= 0);
 
+    String[] namesFilled = new String[arraySize];
 
-String[] namesArray = new String[] { "Damian", "Stanisław", "Karol", "Bartosz", "Dariusz", "Tomasz",
-"Daniel", "Jakub", "Wojciech", "Piotr"};
+    Random randomNumber = new Random();
 
-String[] namesFilled = new String[arraySize];
+    for (int i = 0; i < namesFilled.Length; i++)
+    {
+        namesFilled[i] = namesArray[randomNumber.Next(0, namesArray.Length)];
+    }
 
+    Console.WriteLine("This are all names that has been generated:");
+    int nameNumber = 1;
+    foreach (String names in namesFilled)
+    {
+        Console.WriteLine("Name number " + nameNumber + " is: " + names);
+        nameNumber++;
+    }
+    do
+    {
+        foundNameCounter = 0;
+        foundNameAgain = true;
 
-Random randomNumber = new Random();
+        Console.Write("Enter name to found and I will count how many times it has been found in random names array: ");
+        nameToFound = Console.ReadLine();
+        nameToFound = nameToFound.ToUpper();
 
+        for (int i =0; i< namesFilled.Length;i++) {
+            namesFilled[i] = namesFilled[i].ToUpper();
+        }
+        
+        foreach (String names in namesFilled)
+        {             
+            if (names == nameToFound)
+            {
+                foundNameCounter++;
+            }
+        }
 
-
-for (int i = 0; i < namesFilled.Length; i++)
-{
-    namesFilled[i] = namesArray[randomNumber.Next(0, namesArray.Length)];
-}
-
-foreach (String names in namesFilled)
-{
-    Console.WriteLine(names);
-}
+        Console.WriteLine("Name " + nameToFound + " has been found: " + foundNameCounter + " times in all generated names.");
+        Console.Write("Do You want to found other name? (Y/N)");
+        againFounder = Console.ReadLine();
+        againFounder = againFounder.ToUpper();
+        if (againFounder == "N")
+        {
+            foundNameAgain = false;
+        }
+    } while (foundNameAgain == true);
 }
 catch (Exception ex)
 {
